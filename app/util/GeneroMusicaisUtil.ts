4d9cf1cd460d36e7ae4a74c030a7b9368c886da5
JohnPitter <joaopedro.ts16@gmail.com>
2021-06-22 17:12:41 -0300

    Arquivo com os gêneros musicais

---
+++ app/util/GeneroMusicaisUtil.ts
@@ -0,0 +1,432 @@
+export const GENEROS_MUSICAIS = {
+
+
+    "id": 1,
+    "generos_musicais": [{
+
+        "id": 1,
+        "nome": "Alternativo"
+    },
+    {
+        "id": 2,
+        "nome": "Arrocha"
+    },
+    {
+        "id": 3,
+        "nome": "Axé"
+    },
+    {
+        "id": 4,
+        "nome": "Baião"
+    },
+    {
+        "id": 5,
+        "nome": "Bambuco"
+    },
+    {
+        "id": 6,
+        "nome": "Black Metal"
+    },
+    {
+        "id": 7,
+        "nome": "Blues"
+    },
+    {
+        "id": 8,
+        "nome": "Boi"
+    },
+    {
+        "id": 9,
+        "nome": "Bolero"
+    },
+    {
+        "id": 10,
+        "nome": "Bossa Nova"
+    },
+    {
+        "id": 11,
+        "nome": "Brega"
+    },
+    {
+        "id": 12,
+        "nome": "Caipira"
+    },
+    {
+        "id": 13,
+        "nome": "Chamamé"
+    },
+    {
+        "id": 14,
+        "nome": "Chote"
+    },
+    {
+        "id": 15,
+        "nome": "Clássico"
+    },
+    {
+        "id": 16,
+        "nome": "Coco"
+    },
+    {
+        "id": 17,
+        "nome": "Coral"
+    },
+    {
+        "id": 18,
+        "nome": "Country"
+    },
+    {
+        "id": 19,
+        "nome": "Cover"
+    },
+    {
+        "id": 20,
+        "nome": "Cover Acústico"
+    },
+    {
+        "id": 21,
+        "nome": "Cuarteto"
+    },
+    {
+        "id": 22,
+        "nome": "Cumbia"
+    },
+    {
+        "id": 23,
+        "nome": "Dance"
+    },
+    {
+        "id": 24,
+        "nome": "Death Metal"
+    },
+    {
+        "id": 25,
+        "nome": "Disco"
+    },
+    {
+        "id": 26,
+        "nome": "Eletrônica"
+    },
+    {
+        "id": 27,
+        "nome": "Emocore"
+    },
+    {
+        "id": 28,
+        "nome": "Fado"
+    },
+    {
+        "id": 29,
+        "nome": "Folk"
+    },
+    {
+        "id": 30,
+        "nome": "Forró"
+    },
+    {
+        "id": 31,
+        "nome": "Frevo"
+    },
+    {
+        "id": 32,
+        "nome": "Funk"
+    },
+    {
+        "id": 33,
+        "nome": "Funk Internacional"
+    },
+    {
+        "id": 34,
+        "nome": "Gaúcho"
+    },
+    {
+        "id": 35,
+        "nome": "Gospel"
+    },
+    {
+        "id": 36,
+        "nome": "Grunge"
+    },
+    {
+        "id": 37,
+        "nome": "Guarânia"
+    },
+    {
+        "id": 38,
+        "nome": "Gótico"
+    },
+    {
+        "id": 39,
+        "nome": "Hardcore"
+    },
+    {
+        "id": 40,
+        "nome": "Hard Rock"
+    },
+    {
+        "id": 41,
+        "nome": "Heavy Metal"
+    },
+    {
+        "id": 42,
+        "nome": "Hip Hop"
+    },
+    {
+        "id": 43,
+        "nome": "House"
+    },
+    {
+        "id": 44,
+        "nome": "Indie"
+    },
+    {
+        "id": 45,
+        "nome": "Indie Rock"
+    },
+    {
+        "id": 46,
+        "nome": "Industrial"
+    },
+    {
+        "id": 47,
+        "nome": "Infantil"
+    },
+    {
+        "id": 48,
+        "nome": "Instrumental"
+    },
+    {
+        "id": 49,
+        "nome": "J-Pop"
+    },
+    {
+        "id": 50,
+        "nome": "J-Rock"
+    },
+    {
+        "id": 51,
+        "nome": "Jazz"
+    },
+    {
+        "id": 52,
+        "nome": "Jovem Guarda"
+    },
+    {
+        "id": 53,
+        "nome": "K-Pop"
+    },
+    {
+        "id": 54,
+        "nome": "K-Rock"
+    },
+    {
+        "id": 55,
+        "nome": "Mambo"
+    },
+    {
+        "id": 56,
+        "nome": "Manguebeat"
+    },
+    {
+        "id": 57,
+        "nome": "Marchas"
+    },
+    {
+        "id": 58,
+        "nome": "Mariachi"
+    },
+    {
+        "id": 59,
+        "nome": "Merengue"
+    },
+    {
+        "id": 60,
+        "nome": "Metalcore"
+    },
+    {
+        "id": 61,
+        "nome": "MPB"
+    },
+    {
+        "id": 62,
+        "nome": "Música andina"
+    },
+    {
+        "id": 63,
+        "nome": "Música Pernambucana"
+    },
+    {
+        "id": 64,
+        "nome": "New Age"
+    },
+    {
+        "id": 65,
+        "nome": "New Wave"
+    },
+    {
+        "id": 66,
+        "nome": "Orquestra"
+    },
+    {
+        "id": 67,
+        "nome": "Pagode"
+    },
+    {
+        "id": 68,
+        "nome": "Pop"
+    },
+    {
+        "id": 69,
+        "nome": "Pop Rock"
+    },
+    {
+        "id": 70,
+        "nome": "Post-Rock"
+    },
+    {
+        "id": 71,
+        "nome": "Power-Pop"
+    },
+    {
+        "id": 72,
+        "nome": "Psicodelia"
+    },
+    {
+        "id": 73,
+        "nome": "Punk Rock"
+    },
+    {
+        "id": 74,
+        "nome": "Pós-punk"
+    },
+    {
+        "id": 75,
+        "nome": "R&B"
+    },
+    {
+        "id": 76,
+        "nome": "Ranchera"
+    },
+    {
+        "id": 77,
+        "nome": "Rap"
+    },
+    {
+        "id": 78,
+        "nome": "Reggae"
+    },
+    {
+        "id": 79,
+        "nome": "Reggaeton"
+    },
+    {
+        "id": 80,
+        "nome": "Regional"
+    },
+    {
+        "id": 81,
+        "nome": "Religioso"
+    },
+    {
+        "id": 82,
+        "nome": "Rock"
+    },
+    {
+        "id": 83,
+        "nome": "Rockabilly"
+    },
+    {
+        "id": 84,
+        "nome": "Rock melódico"
+    },
+    {
+        "id": 85,
+        "nome": "Rock Progressivo"
+    },
+    {
+        "id": 86,
+        "nome": "Romântico"
+    },
+    {
+        "id": 87,
+        "nome": "Salsa"
+    },
+    {
+        "id": 88,
+        "nome": "Samba"
+    },
+    {
+        "id": 89,
+        "nome": "Samba Enredo"
+    },
+    {
+        "id": 90,
+        "nome": "Screamo"
+    },
+    {
+        "id": 91,
+        "nome": "Sertanejo"
+    },
+    {
+        "id": 92,
+        "nome": "Sertanejo Universitário"
+    },
+    {
+        "id": 93,
+        "nome": "Sinfonia"
+    },
+    {
+        "id": 94,
+        "nome": "Ska"
+    },
+    {
+        "id": 95,
+        "nome": "Soft Rock"
+    },
+    {
+        "id": 96,
+        "nome": "Soul"
+    },
+    {
+        "id": 97,
+        "nome": "Surf Music"
+    },
+    {
+        "id": 98,
+        "nome": "Tango"
+    },
+    {
+        "id": 99,
+        "nome": "Teclado"
+    },
+    {
+        "id": 100,
+        "nome": "Tecnopop"
+    },
+    {
+        "id": 101,
+        "nome": "Thrash Metal"
+    },
+    {
+        "id": 102,
+        "nome": "Trova"
+    },
+    {
+        "id": 103,
+        "nome": "Velha Guarda"
+    },
+    {
+        "id": 104,
+        "nome": "World Music"
+    },
+    {
+        "id": 105,
+        "nome": "Zamba"
+    },
+    {
+        "id": 106,
+        "nome": "Zouk"
+    }
+]
+
+}
