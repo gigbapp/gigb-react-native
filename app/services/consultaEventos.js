e9b0a010fbb717c054b6a54a5ed1bb965f50518c
Nicolly Figueiredo <nicolly_francine@hotmail.com>
2021-06-30 16:01:35 -0300

    Ajustes finais

--- app/services/consultaEventos.js
+++ app/services/consultaEventos.js
@@ -2,14 +2,8 @@ import axios from 'axios';
 
 export const ConsultaEventosArtistas = async (Id_artista) => {
     const url = `https://apirecomendacaogigb.herokuapp.com/ListaEventos?Id_artista=${Id_artista}`;
-
-    let listaEventos = []
-    
+    let listaEventos = [];
     await axios.get(url).then((sucess) => {listaEventos = sucess.data});
-    
-    /*for (const key in res.data) {
-        listaEventos.push(res.data[key]);   
-    }*/
     
     return listaEventos;
 };
